9508b230760be9d39b95a32a8e64eb0993a1198e
maxposchmann <maxposchmann@gmail.com>
2022-03-03 17:04:26 -0500

    Build up reference data read from entry

--- optimaData.py
+++ optimaData.py
@@ -65,7 +65,7 @@ class TagWindow:
 
 
 class PointValidationWindow:
-    def __init__(self,npoints,elements,points,windowList):
+    def __init__(self,npoints,elements,points,reference,windowList):
         self.windowList = windowList
         self.windowList.append(self)
         self.npoints = npoints
@@ -74,6 +74,7 @@ class PointValidationWindow:
         # This array will have all the previously-accumulated points in it.
         # The plan is to allow multiple of these windows simultaneously.
         self.points = points
+        self.reference = reference
 
         self.open()
         self.children = []
@@ -94,4 +95,87 @@ class PointValidationWindow:
         if event == sg.WIN_CLOSED or event == 'Cancel':
             self.close()
         if event == 'Accept':
-            self.close()
+            # use newpoints to accumulate points entered here
+            newpoints = []
+            temp, status = self.validEntry(values[f'-temp{0}-'])
+            if not (status == 0 and temp >= 300):
+                print('Invalid temperature in line 0')
+                return
+            pres, status = self.validEntry(values[f'-pres{0}-'])
+            if not (status == 0 and pres > 0):
+                print('Invalid pressure in line 0')
+                return
+            concentrations = []
+            for element in self.elements:
+                conc, status = self.validEntry(values[f'-{element}{0}-'])
+                concentrations.append(conc)
+            if not max(concentrations) > 0:
+                print('Need at least one element present')
+                return
+            newpoints.append([[temp,pres]+concentrations])
+            for i in range(1,self.npoints):
+                lastTemp = temp
+                lastPres = pres
+                lastConcentrations = concentrations
+                temp, status = self.validEntry(values[f'-temp{i}-'])
+                if status == 1:
+                    temp = lastTemp
+                elif not (status >= 0 and temp >= 300):
+                    print(f'Invalid temperature in line {i+1}')
+                    return
+                pres, status = self.validEntry(values[f'-pres{i}-'])
+                if status == 1:
+                    pres = lastPres
+                elif not (status >= 0 and pres > 0):
+                    print(f'Invalid pressure in line {i+1}')
+                    return
+                concentrations = []
+                for element in self.elements:
+                    conc, status = self.validEntry(values[f'-{element}{i}-'])
+                    if status == -1:
+                        return
+                    elif status == 1:
+                        conc = lastConcentrations[self.elements.index(element)]
+                    concentrations.append(conc)
+                if not max(concentrations) > 0:
+                    print('Need at least one element present')
+                    return
+                newpoints.append([temp,pres]+concentrations)
+            # Make window to enter reference data
+            headerLayout = [[sg.Text('Gibbs Energy')]]
+            rowLayout = [[sg.Input(key=f'-gibbs{i}-',size=inputSize)] for i in range(self.npoints)]
+            buttonLayout = [[sg.Button('Accept'),sg.Button('Cancel')]]
+            referenceWindow = sg.Window('Reference Data', [headerLayout,rowLayout,buttonLayout], location = [800,0], finalize=True)
+            while True:
+                event, values = referenceWindow.read(timeout=timeout)
+                if event == sg.WIN_CLOSED or event == 'Cancel':
+                    break
+                if event == 'Accept':
+                    newgibbs = []
+                    for i in self.npoints:
+                        try:
+                            gibbs = float(values[f'-gibbs{i}-'])
+                            newgibbs.append(gibbs)
+                        except ValueError:
+                            print(f'Invalid entry -gibbs{i}-')
+                            return
+                    self.points.append(newpoints)
+                    self.reference.append(newgibbs)
+                    self.close()
+            npointsWindow.close()
+    def validEntry(self,value):
+        if value == '':
+            outValue = 0
+            status = 1
+        else:
+            try:
+                outValue = float(value)
+                if outValue >= 0:
+                    status = 0
+                else:
+                    raise ValueError
+            except ValueError:
+                print(f'Invalid entry {value}')
+                outValue = 0
+                status = -1
+        return outValue, status
