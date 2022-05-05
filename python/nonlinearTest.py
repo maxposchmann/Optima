eccc4fa281db9ae0375d0af87d4317e2395e3739
maxposchmann <maxposchmann@gmail.com>
2022-05-05 19:15:03 -0400

    Add more looping for convenience

--- python/nonlinearTest.py
+++ python/nonlinearTest.py
@@ -6,13 +6,14 @@ import optima
 nTests = 100
 nVal = 100
 nTotalParams = 2
-paramRange = [0+i for i in [0,5]]
+xrange = 100
+
 # Optima parameters
 tol = 1e-4
 maxIts = 100
 maxAttempts = 1
 method = optima.LevenbergMarquardtBroyden
-method = optima.Bayesian
+# method = optima.Bayesian
 
 # Create a "black-box" function that meets Optima's requirements
 def nonlinearFunction(validation, tags, beta):
@@ -25,39 +26,45 @@ def nonlinearFunction(validation, tags, beta):
     f = np.array(f)
     return f
 
-nSuccess = 0
-for ti in range(nTests):
-    # Generate validation data using "true" parameter values
-    trueParams = [paramRange[0] + (paramRange[1]-paramRange[0])*(random.random()) for i in range(nTotalParams)]
-    print(trueParams)
-    values = []
-    for n in range(nVal):
-        values.append([random.random() for i in range(nTotalParams)])
+suc = []
+for j in range(0,10):
+    paramRange = [j+i for i in [0,1]]
 
-    # Combine validation data with function
-    def getValues(tags, beta):
-        return nonlinearFunction(values, tags, beta)
+    nSuccess = 0
+    for ti in range(nTests):
+        # Generate validation data using "true" parameter values
+        trueParams = [paramRange[0] + (paramRange[1]-paramRange[0])*(random.random()) for i in range(nTotalParams)]
+        print(trueParams)
+        values = []
+        for n in range(nVal):
+            values.append([xrange * random.random() for i in range(nTotalParams)])
 
-    y = getValues([], trueParams)
+        # Combine validation data with function
+        def getValues(tags, beta):
+            return nonlinearFunction(values, tags, beta)
 
-    # Set up Optima
-    tags = dict([])
-    totalIts = 0
-    for p in range(nTotalParams):
-        tags[str(trueParams[p])] = [paramRange[0],paramRange[1]]
-    for r in range(maxAttempts):
-        try:
-            norm, iterations, beta = method(y,tags,getValues,maxIts,tol)
-            if norm < tol:
-                totalIts += iterations
-                nSuccess += 1
-                break
-        except ValueError:
-            if method == optima.LevenbergMarquardtBroyden:
-                for p in range(nTotalParams):
-                    tags[str(trueParams[p])] = [paramRange[0] + (paramRange[1]-paramRange[0])*(random.random()),paramRange[0] + (paramRange[1]-paramRange[0])*(random.random())]
-        totalIts += maxIts
-    print(trueParams)
-    print(totalIts)
-    print(f' ---- DONE {ti+1}, PASSED {nSuccess} ----')
-print(f'{nSuccess} successes out of {nTests}')
+        y = getValues([], trueParams)
+
+        # Set up Optima
+        tags = dict([])
+        totalIts = 0
+        for p in range(nTotalParams):
+            tags[str(trueParams[p])] = [paramRange[0],paramRange[1]]
+        for r in range(maxAttempts):
+            try:
+                norm, iterations, beta = method(y,tags,getValues,maxIts,tol)
+                if norm < tol:
+                    totalIts += iterations
+                    nSuccess += 1
+                    break
+            except ValueError:
+                if method == optima.LevenbergMarquardtBroyden:
+                    for p in range(nTotalParams):
+                        tags[str(trueParams[p])] = [paramRange[0] + (paramRange[1]-paramRange[0])*(random.random()),paramRange[0] + (paramRange[1]-paramRange[0])*(random.random())]
+            totalIts += maxIts
+        print(trueParams)
+        print(totalIts)
+        print(f' ---- {j} DONE {ti+1}, PASSED {nSuccess} ----')
+    print(f'{nSuccess} successes out of {nTests}')
+    suc.append(nSuccess)
+    print(suc)
