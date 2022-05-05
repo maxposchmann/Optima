d96bad673aba0eb36da98dc3168f47f4bbfceda8
maxposchmann <maxposchmann@gmail.com>
2022-05-05 15:04:52 -0400

    Make parameter range more customizable

--- python/nonlinearTest.py
+++ python/nonlinearTest.py
@@ -3,16 +3,16 @@ import random
 import optima
 
 # Testing parameters
-nTests = 1000
+nTests = 100
 nVal = 100
-nTotalParams = 5
-paramRange = 2
+nTotalParams = 2
+paramRange = [0+i for i in [0,5]]
 # Optima parameters
 tol = 1e-4
 maxIts = 100
 maxAttempts = 1
 method = optima.LevenbergMarquardtBroyden
-# method = optima.Bayesian
+method = optima.Bayesian
 
 # Create a "black-box" function that meets Optima's requirements
 def nonlinearFunction(validation, tags, beta):
@@ -26,9 +26,9 @@ def nonlinearFunction(validation, tags, beta):
     return f
 
 nSuccess = 0
-for n in range(nTests):
+for ti in range(nTests):
     # Generate validation data using "true" parameter values
-    trueParams = [paramRange*(random.random()) for i in range(nTotalParams)]
+    trueParams = [paramRange[0] + (paramRange[1]-paramRange[0])*(random.random()) for i in range(nTotalParams)]
     print(trueParams)
     values = []
     for n in range(nVal):
@@ -44,7 +44,7 @@ for n in range(nTests):
     tags = dict([])
     totalIts = 0
     for p in range(nTotalParams):
-        tags[str(trueParams[p])] = [0,paramRange]
+        tags[str(trueParams[p])] = [paramRange[0],paramRange[1]]
     for r in range(maxAttempts):
         try:
             norm, iterations, beta = method(y,tags,getValues,maxIts,tol)
@@ -55,11 +55,8 @@ for n in range(nTests):
         except ValueError:
             if method == optima.LevenbergMarquardtBroyden:
                 for p in range(nTotalParams):
-                    tags[str(trueParams[p])] = [paramRange*(random.random()),paramRange*(random.random())]
+                    tags[str(trueParams[p])] = [paramRange[0] + (paramRange[1]-paramRange[0])*(random.random()),paramRange[0] + (paramRange[1]-paramRange[0])*(random.random())]
         totalIts += maxIts
-        # else:
-        #     for p in range(nTotalParams):
-        #         tags[str(p)] = [beta[p],beta[p]]
     print(trueParams)
     print(totalIts)
     print(f' ---- DONE {ti+1}, PASSED {nSuccess} ----')
